31284dd2d60284fb86de77f21c5531d740179612
survivorbat <djbatcat@gmail.com>
2020-06-22 12:13:15 +0200

    master: Move to DO token

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,8 +1,14 @@
 terraform {
-  backend "azurerm" {
-    resource_group_name  = "Example"
-    storage_account_name = "examplestate"
-    container_name       = "tfstate"
-    key                  = "prod.terraform.tfstate"
+  backend "s3" {
+    bucket = "example-terraform-state-dev"
+    key = "terraform-state.tfstate"
+    access_key = "__doAccessKey__"
+    secret_key = "__doSecretKey__"
+    endpoint = "https://ams3.digitaloceanspaces.com"
+    region = "eu-west-1"
+    skip_credentials_validation = true
+    skip_get_ec2_platforms = true
+    skip_requesting_account_id = true
+    skip_metadata_api_check = true
   }
 }
