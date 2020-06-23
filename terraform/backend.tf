e105941183dc0f76d8da3594ce5a4e35d91fa762
survivorbat <djbatcat@gmail.com>
2020-06-23 21:07:25 +0200

    master: Remove old attributes

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -7,8 +7,6 @@ terraform {
     endpoint = "https://ams3.digitaloceanspaces.com"
     region = "eu-west-1"
     skip_credentials_validation = true
-    skip_get_ec2_platforms = true
-    skip_requesting_account_id = true
     skip_metadata_api_check = true
   }
 }
